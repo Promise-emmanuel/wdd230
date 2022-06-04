f66a5555879982b9969f3c7c9204ccfb4625dfed
Promise-emmanuel <Promiseemmanuel@byui.edu>
2022-06-04 12:24:16 -0700

    wertwet

--- lesson 6 image/script/windchill.js
+++ lesson 6 image/script/windchill.js
@@ -1,15 +1,15 @@
 const temperature = parseFloat(document.getElementById("temperature").innerHTML);
 
-const windSpeed = parseFloat(document.getElementById("windSpeed").innerHTML);
-const windChill = document.getElementById("windChill");
+const windSpeed = parseFloat(document.getElementById("windspeed").innerHTML);
+const windChill = document.getElementById("windchill");
 
 console.log(temperature)
 console.log(windSpeed)
 
 if (temperature <= 50 || windSpeed < 3) {
 
-    const windChill_f = 35.74 + (0.6215 * temperature) - (35.75 * windSpeed**0.16) + (0.4275 * temperature * windSpeed**0.16);
-    windChill.innerHTML = Math.round(windChill_f);
+    const windChill_formula = 35.74 + (0.6215 * temperature) - (35.75 * windSpeed**0.16) + (0.4275 * temperature * windSpeed**0.16);
+    windChill.innerHTML = Math.round(windChill_formula);
 
 }
 
