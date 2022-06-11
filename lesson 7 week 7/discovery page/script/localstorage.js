7dc0f022aa3081b22e34eb636248ba3dd27de550
Promise-emmanuel <Promiseemmanuel@byui.edu>
2022-06-11 15:50:49 -0700

    ijjjjjj

--- lesson 7 week 7/discovery page/script/localstorage.js
+++ lesson 7 week 7/discovery page/script/localstorage.js
@@ -1,18 +1,21 @@
 const visitsDisplay = document.querySelector(".visits");
+const minute = 1000 * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // get the stored value in localStorage
-let numVisits = Number(window.localStorage.getItem("visits-ls"));
+let lastVisit = Number(window.localStorage.getItem("visits-ls"));
 
-// determine if this is the first visit or display the number of visits.
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
+// determine if this is the first visit or display the number in days you last visited.
+if (lastVisit !== 0) {
+	newVisit = Math.round((Date.now() - lastVisit) /day );
+
+	visitsDisplay.textContent = newVisit;
 } else {
 	visitsDisplay.textContent = `This is your first visit!`;
 }
 
-// increment the number of visits.
 
-// how should this be improved?
-numVisits++;
-// store the new number of visits value
-localStorage.setItem("visits-ls", numVisits);
+// store the new date of visits value in milliseconds such that when it is called above
+newDate = Date.now()
+localStorage.setItem("visits-ls", newDate);
